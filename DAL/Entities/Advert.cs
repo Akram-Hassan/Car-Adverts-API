2d12eda39a45395147abc719640f9026488d8ad4
Akram Hassan <akram.imam82@gmail.com>
2019-02-09 22:31:20 +0200

    New adverts cannot have first registration date

---
+++ DAL/Entities/Advert.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public enum FuelType
+    {
+        Gasoline,
+        Diesel
+    }
+
+    public class Advert
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public FuelType Fuel { get; set; }
+
+        public bool New { get; set; }
+
+        private int? milage;
+        public int? Mileage
+        {
+            get => milage;
+            set
+            {
+                if (value != null && New == true)
+                {
+                    throw new InvalidOperationException("New adverts cannot have milage");
+                }
+                milage = value;
+            }
+        }
+
+
+        private DateTime? firstRegistration;
+        public DateTime? FirstRegistration {
+            get => firstRegistration;
+            set
+            {
+                if (value != null && New == true)
+                {
+                    throw new InvalidOperationException("New adverts cannot have first registration date");
+                }
+                firstRegistration = value;
+            }
+        }
+    }
+}
