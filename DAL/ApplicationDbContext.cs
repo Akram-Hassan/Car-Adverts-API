c40077af9a06f1d0b4ef5c6801d89881715ea3f5
Akram Hassan <akram.imam82@gmail.com>
2019-02-11 06:49:30 +0200

    Add Seed data

--- DAL/ApplicationDbContext.cs
+++ DAL/ApplicationDbContext.cs
@@ -8,9 +8,11 @@ namespace DAL
 {
     public class ApplicationDbContext : DbContext
     {
-        private Advert[] adverts = {
+        private Advert[] seedAdverts = {
             new Advert {ID = 1 , Title = "New Disesel Car Advert" , Fuel = FuelType.Diesel , New = true},
-            new Advert {ID = 2 , Title = "New Gasoline Car Advert" , Fuel = FuelType.Gasoline , New = true}
+            new Advert {ID = 2 , Title = "New Gasoline Car Advert" , Fuel = FuelType.Gasoline , New = true},
+            new Advert {ID = 3 , Title = "Used Diesel Car Advert" , Fuel = FuelType.Diesel , New = false , Mileage = 30 , FirstRegistration = new DateTime(2015,01,01)},
+            new Advert {ID = 4 , Title = "Used Gasoline Car Advert" , Fuel = FuelType.Gasoline , New = false , Mileage = 45 , FirstRegistration = new DateTime(2010,05,01)}
         };
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
@@ -20,7 +22,7 @@ namespace DAL
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Advert>().HasData(adverts);
+            modelBuilder.Entity<Advert>().HasData(seedAdverts);
         }
     }
 }
