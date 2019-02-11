3c8a3f982b7f5ac4cf1e8a1a77414aaaa5241af7
Akram Hassan <akram.imam82@gmail.com>
2019-02-11 08:12:52 +0200

    Create controller view model

--- Car Adverts/Controllers/AdvertsController.cs
+++ Car Adverts/Controllers/AdvertsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Car_Adverts.ViewModels;
 using DAL;
+using DAL.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,9 +21,19 @@ namespace Car_Adverts.Controllers
             this.repository = repository;
         }
 
-        public IEnumerable<string> Get()
+        public IEnumerable<AdvertVM> Get()
         {
-            return repository.GetAll().Select(x => x.Title);
+            return repository.GetAll().Select( a => Map(a) );
         }
+
+        private AdvertVM Map(Advert advert)
+            => new AdvertVM
+            {
+                Title = advert.Title,
+                Fuel = advert.Fuel,
+                New = advert.New,
+                Mileage = advert.Mileage,
+                FirstRegistration = advert.FirstRegistration
+            };
     }
 }
---
+++ Car Adverts/ViewModels/AdvertVM.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car_Adverts.ViewModels
+{
+    public class AdvertVM
+    {
+        public string Title { get; set; }
+        public FuelType Fuel { get; set; }
+        public bool New { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Only positive number allowed")]
+        public int? Mileage { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? FirstRegistration { get; set; }
+    }
+}
